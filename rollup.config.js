1391f496fa3d6e2ff47a0a25150fdb2798f5d74a
Dave Cardwell <hello@davecardwell.com>
2019-10-14 17:32:13 -0400

    fix: add node shebang to allow for npx usage

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,18 @@
+import addShebang from "rollup-plugin-add-shebang";
 import babel from "rollup-plugin-babel";
 import sourcemaps from "rollup-plugin-sourcemaps";
+
+const outputFile = "dist/index.node.js";
 
 export default {
   input: "dist/index.js",
   output: {
-    file: "dist/index.node.js",
+    file: outputFile,
     format: "cjs",
     sourcemap: true,
   },
   external: ["events", "inquirer", "puppeteer"],
-  plugins: [sourcemaps(), babel()],
+  plugins: [sourcemaps(), babel(), addShebang({ include: outputFile })],
   watch: {
     clearScreen: false,
   },
