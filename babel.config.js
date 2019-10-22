7c166f491a91f073e7ab06ae0c1a9f89d898d66f
Dave Cardwell <hello@davecardwell.com>
2019-10-22 16:49:49 -0400

    chore: get node target version from package.json
    Parse the Node.js version for @babel/preset-env to target from the
    `engines` setting in package.json to remove redundancy.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
+const pkg = require("./package.json");
+const semver = require("semver");
+
+const nodeTarget = semver.minVersion(pkg.engines.node).toString();
+
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "6.4.0" } }]],
+  presets: [["@babel/preset-env", { targets: { node: nodeTarget } }]],
   sourceMaps: true,
   inputSourceMap: true,
 };
