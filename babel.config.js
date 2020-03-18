dc4fbf302cd0398991c7a4e71b3135a529b504a9
Dave Cardwell <hello@davecardwell.com>
2020-03-18 10:47:50 -0400

    chore(deps): update babel to v7.8.7
    Remove dependencies on
    @babel/plugin-proposal-nullish-coalescing-operator and
    @babel/plugin-proposal-optional-chaining as these are included in
    @babel/preset-env by default as of v7.8.0.

--- babel.config.js
+++ babel.config.js
@@ -5,10 +5,6 @@ const nodeTarget = semver.minVersion(pkg.engines.node).toString();
 
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: nodeTarget } }]],
-  plugins: [
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining",
-  ],
   sourceMaps: true,
   inputSourceMap: true,
 };
